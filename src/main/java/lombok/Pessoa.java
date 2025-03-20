6b5ba53fd69d2f8c4218be24ca4f6310bb1713d8
OsleneCerqueira <oslene.cerqueira@gmail.com>
2025-03-20 12:04:07 -0300

    exemplos: lombok

---
+++ src/main/java/lombok/Pessoa.java
@@ -0,0 +1,15 @@
+package lombok;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Pessoa {
+
+    private Long id;
+    private String nome;
+    private Integer idade;
+}
